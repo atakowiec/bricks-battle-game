77992661b9b5a056da1eebf2692556585096cefb
atakowiec <mat.cieszczyk@gmail.com>
2024-03-12 12:39:33 +0100

    add docker compose

--- bricks-battle-server/src/auth/auth.module.ts
+++ bricks-battle-server/src/auth/auth.module.ts
@@ -13,7 +13,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
         secret: configService.get('JWT_SECRET'),
-        signOptions: { expiresIn: '20s' },
+        signOptions: { expiresIn: '1h' },
       }),
     }),
   ],
